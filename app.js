5dcc849bf629b3dfdb9b46c7cf0665df38479fd4
jcsamgit <jcv.gerald@gmail.com>
2025-09-15 22:14:22 -0300

    funcion agregar amigos
    se agrego un array para almacenar los nombres, tambien se agrego una funcionalidad para evitar que no se puede enviar un campo vacio o con simbolos y/o numeros

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos= [];
+
+
+function agregarAmigo(){
+    let inputAmigo= document.querySelector("#amigo");
+    let amigo = inputAmigo.value;
+    let regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    
+    if(amigo===""){
+        alert("Por favor, inserte un nombre");
+        return
+    }
+    if (!regex.test(amigo)) {
+        alert("No se permite numeros y/o simbolos. Por favor escriba un nombre como la gente!");
+        return;
+    }
+    amigos.push(amigo);
+    inputAmigo.value="";
+    console.log(amigos);
+}
